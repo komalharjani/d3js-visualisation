0a5d59c7a7b8caa653ac514276b9d98a17bfcb57
amc47@st-andrews.ac.uk <amc47@st-andrews.ac.uk>
2020-05-02 17:35:47 -0400

    komal update

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 //1. What the bar chart should represent -- Data
+//2. Add CSS - colours
 //3. Add Filters
-//1. Average Data {done}
-//2. Draw Bar Chart {done}
+//4. Add Transitions
 
-
-
-// This controls counter displayed
-var counter = 1990;
-document.getElementById("counter").innerHTML = counter;
-
-
-//Year starts at 1990
-var objYear = 1989; 
-
+//1. Average Data
+//2. Draw Bar Chart
 
 let dataPath = "data/energy.csv";
 d3.csv(dataPath)
@@ -21,21 +13,15 @@ d3.csv(dataPath)
                 //Nested Data with all Data Required - need to figure out how to unpack and build bar chart
                 var nestedData = d3.nest()
                         .key(function (d) {
-
-                                if ( d.type == "Total Electric Power Industry") {
-                                        return d.region;
-                                }
-                               
+                                return d.year;
                         })
                         .key(function (d) {
-                                objYear++; //starts in 1990 then goes to next year
-                                return d.year;
-                                
-                                
+                                if (d.region != "combined" && d.type == "Total Electric Power Industry") {
+                                        return d.region;
+                                }
                         })
                         .key(function (d) {
                                 if (d.energySrc == "Coal" || d.energySrc == "Petroleum" || d.energySrc == "Natural Gas" || d.energySrc == "Solar Thermal and Photovoltaic") {
-                                        
                                         return d.energySrc;
                                 }
                         })
@@ -47,77 +33,24 @@ d3.csv(dataPath)
                                 };
                         })
                         .entries(data);
+                console.log(nestedData);
+                let nestedDataJSON = JSON.stringify(nestedData);
 
-                        //regions codes: [0]west, [1]undefined, [2]southeast, [3]southwest, [4]northeast, [5]midwest, [6] "", [7]combined
-                        //year codes: [0]1990 to [28]2018
-                        //energySrc codes: [0]undefined, [1]Coal, [2]Natural Gas, [3]Petroleum, [4]Solar Thermal and Photovoltaic
+                var eachPain = d3.values(nestedData[0]).values[0];
+                console.log(eachPain);
 
-                        console.log(nestedData[0].values[0].values[1].value); // equivalent to: west, 1990, Coal, 11860158.0833
-                        console.log(nestedData[0].values[1].values[1].value); //equivalent to: west, 1991, Coal, 11903090.583333334
+let randomData = [];
 
-
-                        //console.log(nestedData);
-                        //let nestedDataJSON = JSON.stringify(nestedData, null, 2);
-                        //console.log (nestedDataJSON);
-
-
-                        
-                /* Whats this?
-                //var eachPain = d3.values(nestedData[0]).values[0];
-                //console.log(eachPain);
-                */
-                
-
-
-                
-                
-                
-
-
-//VISUAL DATA HERE
-//var r;
-// var e; 
-//allWestArray
-//allMidwestArray
-
-let coalAvgWest = [];
-
-
-        // //Filter coal averages into new obj for all regions
-        for (let i = 0; i < 29; i++) { //years: wont exit until all years are reached
-                /*  //a single region array containing all avg energytypes each year
-                for(r =0; r<8; r++){ //regions interation
-                        (if r = 0){ // [0] region west
-                             for (e=0;e<5; e++ ){ //iterate through energySrc and append to region //No bc, would need to map location of e anyways but all source averages would be in a single array. vars seem more straighforward.
-                                //allAvgWest = nestedData[r].values[i].values[e].value;
-                                allWestArray.push{
-                                        allAvgMidWest;
-                                 
-                        }
-                        
-                             (if r = 1){ //[1] region midwest 
-                                for (e= 0;e<5; e++ ){
-                                //allAvgMidWest = nestedData[r].values[i].values[e].value; 
-                                allMidWestArray.push
-                                        allAvgMidwest //this would be a global
-                                }
-                        
-                        }
-                        (if r = 2){ // [2]region 
-                                //do the same as above until r=5 and dont forget else
-
-                }
+// //Filter Data into new object
+for (let i = 0; i < data.length; i++) {
+        if (data[i].year == 1990 && data[i].region == "west" && data[i].type == "Total Electric Power Industry") {
+                randomData.push({
+                        region: data[i].region,
+                        amount: parseInt(data[i].amount),
+                        type: data[i].energySrc,
+                })
         }
-                */
-                var coalData = nestedData[0].values[i].values[1].value; // testing west[0], iterate through years,coal stays at 1.
-
-                coalAvgWest.push({
-                        coalData     
-                })   
-        }
-        console.log(coalAvgWest); //prints all averages for coal each year in west.
-
-
+}
 
 let width = 1000;
 let height = 500;
@@ -141,11 +74,11 @@ let svg = d3.select("body")
 
 
 let yscale = d3.scaleLinear()
-        .domain([0, d3.max(coalAvgWest, d => d.amount)])
+        .domain([0, d3.max(randomData, d => d.amount)])
         .range([0, height]);
 
 let yAxisScale = d3.scaleLinear()
-        .domain([0, d3.max(coalAvgWest, d => d.amount)])
+        .domain([0, d3.max(randomData, d => d.amount)])
         .range([height, 0]);
 
 // Y Axis - Number
