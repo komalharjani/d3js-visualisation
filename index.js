79914e105cf6c52f1535697013885f158be76247
amc47@st-andrews.ac.uk <amc47@st-andrews.ac.uk>
2020-05-05 22:05:13 -0400

    More cleaning up

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ d3.csv(dataPath)
                 }
             })
             .key(function (d) {
-                if (d.energySrc == "Coal" || d.energySrc == "Petroleum" || d.energySrc == "Natural Gas" || d.energySrc == "Solar Thermal and Photovoltaic") {
+                if (d.energySrc == "Coal" || d.energySrc == "Nuclear" || d.energySrc == "Natural Gas" || d.energySrc == "Solar Thermal and Photovoltaic" || d.energySrc == "wind") {
                     return d.energySrc;
                 }
             })
+
+
             //Form Averages for Each Year
             .rollup(function (v) {
                 return {
@@ -30,228 +32,216 @@ d3.csv(dataPath)
 
 
 
-            var slider = document.getElementById("myRange");
-            var output = document.getElementById("demo");
-            output.innerHTML = slider.value;
-            
-            var svg,x0,x1,y;
-            
-      
-        
 
 
 
 
+        var slider = document.getElementById("myRange");
+        var output = document.getElementById("demo");
+        //output.innerHTML = slider.value;
 
 
-
+        //FILTERED DATA BY REGION
         //collects filterted region by year data and renders graph
-        slider.onchange = function() {
-                var selectedYear = document.getElementById("myRange").value;
-                output.innerHTML = this.value;
-               
+        slider.onchange = function () {
+            var margin,svg, x0, x1, y, xAxis, yAxis, regionNames, energyNames;
+
+            var selectedYear = document.getElementById("myRange").value;
+            output.innerHTML = this.value;
+
 
             //change d.key -- based on dropdown input value
-        let filteredData = [];
-        let getNestedData = nestedData.forEach(function (d) { //d = year
-            if (d.key == selectedYear) { //IF WANT ALL DATA FOR ALL YEARS REMOVE THIS
-                console.log(selectedYear);
-                (d.values).forEach(function (e) { //e = region
-                    (e.values).forEach(function (f) { //f = energySrc
-                        if (e.key !== "undefined" && f.key !== "undefined") {
-                                
-                            filteredData.push({
-                                region: e.key,
-                                year: d.key,
-                                energy: f.key,
-                                avg: f.value.avg,
-                            })
-                        }
+            let filteredData = [];
+            let getNestedData = nestedData.forEach(function (d) { //d = year
+                if (d.key == selectedYear) { //IF WANT ALL DATA FOR ALL YEARS REMOVE THIS
+                    console.log(selectedYear);
+                    (d.values).forEach(function (e) { //e = region
+                        (e.values).forEach(function (f) { //f = energySrc
+                            if (e.key !== "undefined" && f.key !== "undefined") {
+
+                                filteredData.push({
+                                    region: e.key,
+                                    year: d.key,
+                                    energy: f.key,
+                                    avg: f.value.avg,
+                                })
+                            }
+                        })
                     })
-                })
-           }
-        });
-
-
-
-
-
-
-    //selected data to be visualize
-    let currData = d3.nest() 
-    .key(function (d) {
-        console.log("THIS IS: " +d.year);
-        return d.region;
-    })
-    .entries(filteredData);
-        console.log(currData);
-        console.log(selectedYear);
-        d3.select("svg").remove();
-        
-
-         
-     
-     //SVG section
-     //size of svg (scale, legend, and bar graph)
-     var margin = { top: 20, right: 20, bottom: 30, left: 100 },
-     width = 800 - margin.left - margin.right,
-     height = 400 - margin.top - margin.bottom;
-     
-     
-     x0 = d3.scaleBand().rangeRound([0, width], .5);
-     x1 = d3.scaleBand();
-     y = d3.scaleLinear().rangeRound([height, 0]);
-     
-     var xAxis = d3.axisBottom().scale(x0)
-     .tickFormat(currData.key)
-     .tickValues(currData.map(d => d.key));
-     
-     var yAxis = d3.axisLeft().scale(y);
-     
-     const color = d3.scaleOrdinal(d3.schemeCategory10);
-     
-                 
-                 svg = d3.select('body').append("svg")
-                 .attr("width", width + margin.left + margin.right)
-                 .attr("height", height + margin.top + margin.bottom)
-                 .append("g")
-                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-     
-             var categoriesNames = currData.map(function (d) { return d.key; });
-             var rateNames = currData[0].values.map(function (d) { return d.energy; });
-     
-             x0.domain(categoriesNames);
-             x1.domain(rateNames).rangeRound([0, x0.bandwidth()]);
-             y.domain([0, d3.max(currData, function (key) { return d3.max(key.values, function (d) { return d.avg; }); })]);
-     
-             console.log(y.domain());
-     
-             console.log(currData.key);
-     
-            
-             svg.append("g")
-                 .attr("class", "x axis") //x axis point for instance
-                 .attr("transform", "translate(0," + height + ")")
-                 .call(xAxis);
-             
-     
-             svg.append("g")
-                 .attr("class", "y axis") //y axis point for instance
-                 .style('opacity', '0')
-                 .call(yAxis)
-                 .append("text")
-                 .attr("transform", "rotate(-90)")
-                 .attr("y", 6)
-                 .attr("dy", ".71em")
-                 .style("text-anchor", "end")
-                 .style('font-weight', 'bold')
-                 .text("Value");
-     
-             svg.select('.y').transition().duration(500).delay(1300).style('opacity', '1');
-     
-         
-             
-
-    
-        var durations = 0
-        var afterLoad = () => durations = 750;
-        
-        updateGraph();
-
-
-
-
-
-        //UPDATES THE GRAPH
-        //update graph per selectedYear
-        function updateGraph(){
-        
-      
-
-        var slice = svg.selectAll(".slice")
-        .data([data],function(d){ return d.ser1 });
-
-        //gathers data of all 5 regions and displays by selectedYear
-        slice = slice
-            .data(currData)
-            .enter()
-            .append("g")
-            .attr("class", "slice") //change slice back to "g"
-            .attr("transform", function (d) { return "translate(" + x0(d.key) + ",0)"; }) //gather next 4 regions
-            .merge(slice);
-
-            slice.exit().remove()
-
-            slice.transition()
-            .duration(durations)
-            .attr("bX1", function(d, i) { return x0(d.selectedYear) }) //ser 1 is current slider year
-            .attr("bX2", function(d, i) { return x1(d.selectedYear) }) 
-            .attr("bY", function(d) { return y(d.selectedYear) }) //ser 2 is the next year selected on slider
-      
-
-
-
-               // Call the X axis
-    svg.selectAll(".axis.axis--x").transition()
-    .duration(durations)
-    .call(xAxis);
-
-  // Call the Y axis
-  svg.selectAll(".axis.axis--y").transition()
-    .duration(durations)
-     .call(yAxis);
-
-
-        //Add data points and tool tips
-        slice.selectAll("rect")
-            .data(function (d) { return d.values; })
-            .enter().append("rect")
-            .attr("width", x1.bandwidth())
-            .attr("x", function (d) { return x1(d.energy); })
-            .style("fill", function (d) { return color(d.energy) })
-            .attr("y", function (d) { return y(0); })
-            .attr("height", function (d) { return height - y(0); })
-            .on("mouseover", function (d) {
-                d3.select(this).style("fill", d3.rgb(color(d.energy)).darker(2));
-            })
-            .on("mouseout", function (d) {
-                d3.select(this).style("fill", color(d.energy));
+                }
             });
 
-       
-        slice.selectAll("rect")
-            .transition()
-            .delay(function (d) { return Math.random() * 1000; })
-            .duration(1000)
-            .attr("y", function (d) { return y(d.avg); })
-            .attr("height", function (d) { return height - y(d.avg); });
 
-        var legend = svg.selectAll(".legend")
-            .data(currData[0].values.map(function (d) { return d.energy; }))
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; })
-            .style("opacity", "0");
+            //selectedYear data to be visualize
+            let currData = d3.nest()
+                .key(function (d) {
+                    console.log("THIS IS: " + d.year);
+                    return d.region;
+                })
+                .entries(filteredData);
+            console.log(currData);
+            console.log(selectedYear);
+            d3.select("svg").remove();
 
 
-        legend.append("rect")
-            .attr("x", width - 18)
-            .attr("width", 18)
-            .attr("height", 18)
-            .style("fill", function (d) { return color(d); });
 
-        legend.append("text")
-            .attr("x", width - 24)
-            .attr("y", 9)
-            .attr("dy", ".35em")
-            .style("text-anchor", "end")
-            .text(function (d) { return d; });
 
-        legend.transition().duration(500).delay(function (d, i) { return 1300 + 100 * i; }).style("opacity", "1");
-        
+
+            //SCREEN SETTING and SVG
+            //append the svg obgect to the body of the page
+            //size of svg (scale, legend, and bar graph)
+                margin = { top: 20, right: 20, bottom: 30, left: 100 },
+                width = 800 - margin.left - margin.right,
+                height = 400 - margin.top - margin.bottom;
+
+            x0 = d3.scaleBand().rangeRound([0, width], .5);
+            x1 = d3.scaleBand();
+            y = d3.scaleLinear().rangeRound([height, 0]);
+
+            xAxis = d3.axisBottom().scale(x0)
+                .tickFormat(currData.key)
+                .tickValues(currData.map(d => d.key));
+
+            yAxis = d3.axisLeft().scale(y);
+
+            const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+
+            svg = d3.select('body').append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            regionNames = currData.map(function (d) { return d.key; }); //region names
+            energyNames = currData[0].values.map(function (d) { return d.energy; }); //energy names
+           
+
+            x0.domain(regionNames);
+            x1.domain(energyNames).rangeRound([0, x0.bandwidth()]);
+            y.domain([0, d3.max(currData, function (key) { return d3.max(key.values, function (d) { return d.avg; }); })]);
+
+            console.log(y.domain());
+
+            console.log(currData.key);
+
+
+            svg.append("g")
+                .attr("class", "x axis") //x axis point for instance
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+
+            svg.append("g")
+                .attr("class", "y axis") //y axis point for instance
+                .style('opacity', '0')
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .style('font-weight', 'bold')
+                .text("Value");
+
+            svg.select('.y').transition().duration(500).delay(1300).style('opacity', '1');
+
+
+
+            var durations = 0
+            var afterLoad = () => durations = 750;
+
+            updateGraph();
+
+
+
+            //UPDATES THE GRAPH
+            //update graph per selectedYear
+            function updateGraph() {
+
+                var slice = svg.selectAll(".slice")
+                    .data(currData);
+
+                //gathers data of all 5 regions and displays by selectedYear
+                slice = slice
+                    .enter()
+                    .append("g")
+                    .attr("class", "slice") //change slice back to "g"
+                    .attr("transform", function (d) { return "translate(" + x0(d.key) + ",0)"; }) //gather next 4 regions
+                    .merge(slice);
+
+
+                //Attempt to make smooth transition from graph to new graph
+                slice.transition()
+                    .duration(durations)
+                    .attr("bX1", function (d, i) { return x0(d.currData) }) //ser 1 is current slider year
+                    .attr("bX2", function (d, i) { return x1(d.currData) })
+                    .attr("bY", function (d) { return y(d.currData) }) //ser 2 is the next year selected on slider
+
+
+
+
+                // Call the X axis
+                svg.selectAll(".axis.axis--x").transition()
+                    .duration(durations)
+                    .call(xAxis);
+
+                // Call the Y axis
+                svg.selectAll(".axis.axis--y").transition()
+                    .duration(durations)
+                    .call(yAxis);
+
+
+                //Add data points and tool tips
+                slice.selectAll("rect")
+                    .data(function (d) { return d.values; })
+                    .enter().append("rect")
+                    .attr("width", x1.bandwidth())
+                    .attr("x", function (d) { return x1(d.energy); })
+                    .style("fill", function (d) { return color(d.energy) })
+                    .attr("y", function (d) { return y(0); })
+                    .attr("height", function (d) { return height - y(0); })
+                    .on("mouseover", function (d) {
+                        d3.select(this).style("fill", d3.rgb(color(d.energy)).darker(2));
+                    })
+                    .on("mouseout", function (d) {
+                        d3.select(this).style("fill", color(d.energy));
+                    });
+
+
+                slice.selectAll("rect")
+                    .transition()
+                    .delay(function (d) { return Math.random() * 1000; })
+                    .duration(1000)
+                    .attr("y", function (d) { return y(d.avg); })
+                    .attr("height", function (d) { return height - y(d.avg); });
+
+                var legend = svg.selectAll(".legend")
+                    .data(currData[0].values.map(function (d) { return d.energy; }))
+                    .enter().append("g")
+                    .attr("class", "legend")
+                    .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; })
+                    .style("opacity", "0");
+
+
+                legend.append("rect")
+                    .attr("x", width - 18)
+                    .attr("width", 18)
+                    .attr("height", 18)
+                    .style("fill", function (d) { return color(d); });
+
+                legend.append("text")
+                    .attr("x", width - 24)
+                    .attr("y", 9)
+                    .attr("dy", ".35em")
+                    .style("text-anchor", "end")
+                    .text(function (d) { return d; });
+
+                legend.transition().duration(500).delay(function (d, i) { return 1300 + 100 * i; }).style("opacity", "1");
+
+            }
+            afterLoad();
+
         }
-        afterLoad();
 
-    }
-        
     });
